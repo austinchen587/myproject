cb46af3f7b8554b3a84acfd3f7e02a4d78775154
austinchen587 <austinchen587@gmail.com>
2025-02-08 17:41:49 +0800

    Reapply "16.6"
    This reverts commit a395b2d4385bc3012a11c13591c49940c777d562.

--- crm_backend/customers/views/__init__.py
+++ crm_backend/customers/views/__init__.py
@@ -1,7 +1,7 @@
 # /home/myproject/crm_backend/customers/views/__init__.py
 
 # 导入各个功能模块的视图函数
-from .views_customer_list import customerlist
+from .views_customer_list import customerlist,check_new_comments
 from .views_daily_report import daily_report
 from .views_customer_add import add_customer
 from .views_customer_edit import edit_customer
@@ -23,4 +23,5 @@ __all__ = [
     "analysis_data_json",
     "product_manager_daily_report",
     "get_completion_data",
+    "check_new_comments",
 ]
