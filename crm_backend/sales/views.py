f15d91c65211f08ae37c311da1411d7ba481c8b8
austinchen587 <austinchen587@gmail.com>
2025-02-18 22:05:08 +0800

    18.90

--- crm_backend/sales/views.py
+++ crm_backend/sales/views.py
@@ -1,8 +1,7 @@
-# sales/views.py
-
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from .models import SalesUser  # 假设你的 User 模型在该文件中
 
 def home(request):
     return render(request, 'home.html')
@@ -14,8 +13,12 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         
         if user is not None:
-            login(request, user)
-            return redirect('dashboard')  # 登录成功后重定向到选择页面
+            # 检查用户状态
+            if user.status == 'active':
+                login(request, user)
+                return redirect('dashboard')  # 登录成功后重定向到选择页面
+            else:
+                messages.error(request, '账户已离职，无法登录')  # 如果是离职状态，提示信息
         else:
             messages.error(request, '用户名或密码错误')
     
